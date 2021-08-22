df7535a6cc9ac57d93d464fb3d45b9b4ab1d0acd
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2021-08-22 10:09:14 -0500

    feat create entity user and config

---
+++ src/main/java/jvm/pablohdz/tweetapp/user/User.java
@@ -0,0 +1,86 @@
+package jvm.pablohdz.tweetapp.user;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(name = "user_username")
+    private String userName;
+
+    @Column(name = "user_name")
+    private String firstName;
+
+    @Column(name = "user_email")
+    private String email;
+
+    @Column(name = "user_password")
+    private String password;
+
+    public User() {
+    }
+
+    public User(String userName, String firstName, String email, String password) {
+        this.userName = userName;
+        this.firstName = firstName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", userName='" + userName + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
