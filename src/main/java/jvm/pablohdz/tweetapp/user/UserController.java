613a63c12b1361648c7613e5235904dc11f6e76d
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2021-08-22 11:39:57 -0500

    feat first endpoint for save user is on

---
+++ src/main/java/jvm/pablohdz/tweetapp/user/UserController.java
@@ -0,0 +1,19 @@
+package jvm.pablohdz.tweetapp.user;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/user")
+public class UserController {
+
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    private User createUser(@RequestBody User user) {
+        return userService.addUser(user);
+    }
+}
