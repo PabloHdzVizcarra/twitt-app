aac91837c6c40a1ba9f05189d2863b1f709a6836
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2021-08-22 11:39:37 -0500

    feat create interface and implementation userService

---
+++ src/main/java/jvm/pablohdz/tweetapp/user/UserService.java
@@ -0,0 +1,13 @@
+package jvm.pablohdz.tweetapp.user;
+
+public interface UserService {
+
+
+    /**
+     * Stores a user in the database
+     *
+     * @param user An {@link User} object.
+     * @return the user stored in the database.
+     */
+    User addUser(User user);
+}
---
+++ src/main/java/jvm/pablohdz/tweetapp/user/UserServiceImpl.java
@@ -0,0 +1,18 @@
+package jvm.pablohdz.tweetapp.user;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService{
+
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User addUser(User user) {
+        return userRepository.save(user);
+    }
+}
