96cc473692cd4451690050e82d8d46af5d4565d7
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2021-08-23 08:50:17 -0500

    feat added log system

--- src/main/java/jvm/pablohdz/tweetapp/TweetAppApplication.java
+++ src/main/java/jvm/pablohdz/tweetapp/TweetAppApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TweetAppApplication {
 
-    // TODO: 8/22/21 Create Users
+
     public static void main(String[] args) {
         SpringApplication.run(TweetAppApplication.class, args);
     }
---
+++ src/main/java/jvm/pablohdz/tweetapp/util/LoggingUtil.java
@@ -0,0 +1,20 @@
+package jvm.pablohdz.tweetapp.util;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.logging.LogManager;
+
+@Configuration
+public class LoggingUtil {
+
+    @Bean
+    public void initLogManager() throws IOException {
+        LogManager.getLogManager()
+                .readConfiguration(
+                        new FileInputStream("src/main/resources/logging.properties"));
+    }
+}
