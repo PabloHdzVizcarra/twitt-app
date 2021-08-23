1ce6c8c687f1eed020edb93b3337eb2affbbd973
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2021-08-23 09:05:22 -0500

    feat added logs to UserService

--- src/main/java/jvm/pablohdz/tweetapp/user/UserServiceImpl.java
+++ src/main/java/jvm/pablohdz/tweetapp/user/UserServiceImpl.java
@@ -1,12 +1,16 @@
 package jvm.pablohdz.tweetapp.user;
 
 import jvm.pablohdz.tweetapp.exception.DataDuplicateException;
+
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 @Service
 public class UserServiceImpl implements UserService{
-
+    private static final Logger LOGGER = Logger.getLogger(UserServiceImpl.class.getName());
     private final UserRepository userRepository;
 
     public UserServiceImpl(UserRepository userRepository) {
@@ -16,7 +20,13 @@ public class UserServiceImpl implements UserService{
     @Override
     public User addUser(User user) {
         try {
-            return userRepository.save(user);
+            User userSaved = userRepository.save(user);
+            LOGGER.log(
+                    Level.INFO,
+                    "The user with the email: " + userSaved.getEmail()
+                            + " was registered in the database");
+
+            return userSaved;
         } catch (DataIntegrityViolationException exception) {
             throw  new DataDuplicateException(user.getEmail());
         }
